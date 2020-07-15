3b5a87c5420e6565cf1ec38a85c8d20d3c785d14
Chris8018 <tranuytrieuvi@protonmail.com>
2020-07-15 13:32:37 +0700

    update

--- others.m
+++ others.m
@@ -1,2 +1,14 @@
 # Change octave prompt with
 # PS1('<SOMETHING>');
+
+# Assignment
+a = 1;
+
+# PI value
+a = pi;
+
+# Display Var
+disp(a);
+
+# Format string
+disp(sprintf('2 decimals: %0.2f', a));
